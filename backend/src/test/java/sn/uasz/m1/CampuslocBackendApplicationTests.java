711867f33e8affb832582159096bdd65f3f8cfc4
eadarak00 <eadarak2001@gmail.com>
2025-05-31 01:10:00 +0000

    initialisation du backend

---
+++ backend/src/test/java/sn/uasz/m1/CampuslocBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.uasz.m1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampuslocBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
