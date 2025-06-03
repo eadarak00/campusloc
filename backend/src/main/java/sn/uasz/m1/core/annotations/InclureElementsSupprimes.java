da0e8dd68ac880fd63c7ee2e1a5179bdcfcbf4c4
eadarak00 <eadarak2001@gmail.com>
2025-06-03 21:47:05 +0000

    feat(filter): ajout d'une annotation @InclureElementsSupprimes avec aspect pour désactiver temporairement le filtre 'supprimeFilter' - Création de l'annotation personnalisée @InclureElementsSupprimes - Ajout d'un aspect AOP qui intercepte les méthodes annotées pour désactiver puis réactiver le filtre 'supprimeFilter' - Permet de récupérer les entités marquées comme supprimées (soft delete) de manière contrôlée

---
+++ backend/src/main/java/sn/uasz/m1/core/annotations/InclureElementsSupprimes.java
@@ -0,0 +1,8 @@
+package sn.uasz.m1.core.annotations;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface InclureElementsSupprimes {
+}
