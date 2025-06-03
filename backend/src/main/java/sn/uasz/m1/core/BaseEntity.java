cc9b12b64cd540e3e968a0ba4c7f9ddd5b8a0be3
eadarak00 <eadarak2001@gmail.com>
2025-06-03 12:32:57 +0000

    feat(core) : création de la classe BaseEntity pour les mappings JPA partagés

---
+++ backend/src/main/java/sn/uasz/m1/core/BaseEntity.java
@@ -0,0 +1,30 @@
+package sn.uasz.m1.core;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "cree_a", updatable = false)
+    private LocalDateTime creerA;
+
+
+    @LastModifiedDate
+    @Column(name = "modifiee_a")
+    private LocalDateTime modifierA;
+    
+}
