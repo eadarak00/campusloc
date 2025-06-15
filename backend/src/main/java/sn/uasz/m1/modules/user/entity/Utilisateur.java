993920534625c25d9c85fc52ae742eb2f1c9a4d0
eadarak00 <eadarak2001@gmail.com>
2025-06-15 21:51:01 +0000

    feat : creation de la classe utilisateur

---
+++ backend/src/main/java/sn/uasz/m1/modules/user/entity/Utilisateur.java
@@ -0,0 +1,96 @@
+package sn.uasz.m1.modules.user.entity;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import sn.uasz.m1.core.base.BaseEntity;
+
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Utilisateur extends BaseEntity implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+
+    private String prenom;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String motDePasse;
+
+    private String telephone;
+
+    private LocalDate derniereConnexion;
+
+    private boolean actif = false;
+
+    private boolean bloque = false;
+
+    @Column(name = "photo_profil")
+    private String photoProfil;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    //Implémentation des méthodes de UserDetails
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(() -> role.getNom());
+    }
+
+    @Override
+    public String getPassword() {
+        return motDePasse;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true; 
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !bloque;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return actif;
+    }
+}
