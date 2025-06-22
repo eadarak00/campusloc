5562a15f350d900cff186948738e1b4f09d523e1
eadarak00 <eadarak2001@gmail.com>
2025-06-22 00:20:29 +0000

    feat(security): ajout de UserDetailsService et configuration Spring Security JWT

--- backend/src/main/java/sn/uasz/m1/modules/auth/security/SecurityConfig.java
+++ backend/src/main/java/sn/uasz/m1/modules/auth/security/SecurityConfig.java
@@ -2,37 +2,48 @@ package sn.uasz.m1.modules.auth.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
+import sn.uasz.m1.modules.user.service.UtilisateurDetailsService;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtFilter;
+    private final UtilisateurDetailsService utilisateurDetailsService;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(AbstractHttpConfigurer:: disable)
+                .csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                    // .requestMatchers("/api/v1/auth/**").permitAll()
-                    .requestMatchers("/v1/auth/**").permitAll()
-                    .anyRequest().authenticated()
-                )
+                        .requestMatchers("/v1/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .userDetailsService(utilisateurDetailsService)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
-
-    } 
+    }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return  new BCryptPasswordEncoder();
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // Bean requis pour injecter AuthenticationManager
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
