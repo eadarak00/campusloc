6d0a63baf7d1350cde3d19627cab1b1a09fa187b
eadarak00 <eadarak2001@gmail.com>
2025-06-23 23:18:19 +0000

    feat: creation de la page d'accueil

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import EnConstruction from "../components/EnConstruction";
+
+const Home = () => {
+    return (
+        <>
+            <EnConstruction nomPage="Accueil" />
+        </>
+    )
+}
+export default Home;
