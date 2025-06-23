c6326fb49f3183acbbfa5160f43f37b78e202285
eadarak00 <eadarak2001@gmail.com>
2025-06-23 23:53:15 +0000

    feat : ajout de la retour

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ROUTES from './routes'
+import ROUTES from './routes';
 import Home from "../pages/Home";
 
 const AppRoute = () => {
-    <Router>
-        <Routes>
-             <Route path={ROUTES.HOME} element={<Home />} />
-            
-        </Routes>
-    </Router>
-}
-export default AppRouter;
+    return ( 
+        <Router>
+            <Routes>
+                <Route path={ROUTES.HOME} element={<Home />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default AppRoute;
