9c2767ab66cd74097291ee9945d5e640f214d098
eadarak00 <eadarak2001@gmail.com>
2025-06-23 21:49:40 +0000

    chore: documentation de l'api avec swagger

--- backend/src/main/java/sn/uasz/m1/modules/auth/security/SecurityConfig.java
+++ backend/src/main/java/sn/uasz/m1/modules/auth/security/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/v1/auth/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated())
                 .userDetailsService(utilisateurDetailsService)
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
