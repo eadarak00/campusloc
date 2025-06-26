099026305878b876d50d7bdfec089f48eb2a2997
eadarak00 <eadarak2001@gmail.com>
2025-06-26 11:10:52 +0000

    feat: ajout de la page connexion

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ROUTES from './routes';
 import Home from '../pages/Home';
+import Connexion from '../pages/auth/Connexion';
 
 const AppRouter = () => (
   <Router>
     <Routes>
       <Route path={ROUTES.HOME} element={<Home />} />
-
+      <Route path={ROUTES.CONNEXION} element={<Connexion />} />
     </Routes>
   </Router>
 );
