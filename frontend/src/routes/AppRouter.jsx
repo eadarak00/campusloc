b1a1117b4aeee3f670c91290b42b7a63b387b47c
eadarak00 <eadarak2001@gmail.com>
2025-06-26 16:00:40 +0000

    feat: modification des routes et creation du pubic Layout

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -2,12 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ROUTES from './routes';
 import Home from '../pages/Home';
 import Connexion from '../pages/auth/Connexion';
+import PublicLayout from '../layouts/PublicLayout';
 
 const AppRouter = () => (
   <Router>
     <Routes>
-      <Route path={ROUTES.HOME} element={<Home />} />
-      <Route path={ROUTES.CONNEXION} element={<Connexion />} />
+      <Route path="/" element={<PublicLayout />}>
+        <Route index element={<Home />} />
+        <Route path={ROUTES.CONNEXION} element={<Connexion />} />
+      </Route>
     </Routes>
   </Router>
 );
