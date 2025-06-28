4f0a21f28b89ea8ff6375eb29481126177199a8d
eadarak00 <eadarak2001@gmail.com>
2025-06-28 22:45:04 +0000

    feat: ajout de la route de la page d'inscription

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -3,6 +3,7 @@ import ROUTES from './routes';
 import Home from '../pages/Home';
 import Connexion from '../pages/auth/Connexion';
 import PublicLayout from '../layouts/PublicLayout';
+import Inscription from '../pages/auth/Inscription';
 
 const AppRouter = () => (
   <Router>
@@ -10,6 +11,7 @@ const AppRouter = () => (
       <Route path="/" element={<PublicLayout />}>
         <Route index element={<Home />} />
         <Route path={ROUTES.CONNEXION} element={<Connexion />} />
+        <Route path={ROUTES.INSCRIPTION} element={<Inscription />} />
       </Route>
     </Routes>
   </Router>
