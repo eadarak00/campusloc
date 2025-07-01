7d032995728ec6760f1544cddaf64fe5b48ab522
eadarak00 <eadarak2001@gmail.com>
2025-07-01 23:57:34 +0000

    feat: creation d'un DTo pour la recuperation d'une annonce

---
+++ backend/src/main/java/sn/uasz/m1/modules/annonce/dto/AnnonceResponseDTO.java
@@ -0,0 +1,35 @@
+package sn.uasz.m1.modules.annonce.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+import sn.uasz.m1.modules.annonce.emuns.StatutAnnonce;
+import sn.uasz.m1.modules.annonce.emuns.TypeDeLogement;
+
+@Getter
+@Setter
+public class AnnonceResponseDTO {
+
+    private Long id;
+
+    private String titre;
+    private String description;
+    private TypeDeLogement typeDeLogement;
+    private Double prix;
+
+    private String adresse;
+    private String ville;
+
+    private int surface;
+    private int nombreDeChambres;
+    private int salleDeBains;
+    private int capacite;
+
+    private LocalDateTime datePublication;
+    private StatutAnnonce statut;
+
+    private Long proprietaireId;
+    private String nomProprietaire;
+    private String emailProprietaire;
+}
