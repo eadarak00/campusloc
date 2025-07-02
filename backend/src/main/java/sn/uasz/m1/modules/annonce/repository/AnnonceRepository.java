5178497899980aa67dc9c1e015fc3383835f0e98
eadarak00 <eadarak2001@gmail.com>
2025-07-02 00:00:17 +0000

    feat:  ajout de methode dans le repo de Annonce

--- backend/src/main/java/sn/uasz/m1/modules/annonce/repository/AnnonceRepository.java
+++ backend/src/main/java/sn/uasz/m1/modules/annonce/repository/AnnonceRepository.java
@@ -1,11 +1,26 @@
 package sn.uasz.m1.modules.annonce.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import sn.uasz.m1.modules.annonce.emuns.TypeDeLogement;
 import sn.uasz.m1.modules.annonce.entities.Annonce;
 
 @Repository
 public interface AnnonceRepository extends JpaRepository<Annonce, Long> {
-    
+     List<Annonce> findByProprietaireId(Long id);
+
+    List<Annonce> findByVilleIgnoreCase(String ville);
+
+    List<Annonce> findByTypeDeLogementAndPrixBetween(TypeDeLogement type, Double minPrix, Double maxPrix);
+
+    @Query("SELECT a FROM Annonce a WHERE " +
+           "(:type IS NULL OR a.typeDeLogement = :type) AND " +
+           "(:ville IS NULL OR LOWER(a.ville) = LOWER(:ville)) AND " +
+           "(:minPrix IS NULL OR a.prix >= :minPrix) AND " +
+           "(:maxPrix IS NULL OR a.prix <= :maxPrix)")
+    List<Annonce> rechercherAnnonces(TypeDeLogement type, String ville, Double minPrix, Double maxPrix);
 }
