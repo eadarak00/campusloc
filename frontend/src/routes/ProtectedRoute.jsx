7d6fce2b2c1aa58abde82f5817b95eda77907cc0
eadarak00 <eadarak2001@gmail.com>
2025-07-05 23:20:52 +0000

    feat(routing) : déclarer les routes pour les différentes pages du menu

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -1,10 +1,42 @@
+// import { Navigate } from 'react-router-dom';
+// import ROUTES from './routes';
+
+// const ProtectedRoute = ({ children }) => {
+//   const isAuthenticated = !!localStorage.getItem('token');
+
+//   return isAuthenticated ? children : <Navigate to={ROUTES.CONNEXION} replace />;
+// };
+
+// export default ProtectedRoute;
+
 import { Navigate } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode';
 import ROUTES from './routes';
 
-const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = !!localStorage.getItem('token');
+/**
+ * @param {ReactNode} children - les composants protégés
+ * @param {string[]} allowedRoles - rôles autorisés (ex: ['ADMIN', 'BAILLEUR'])
+ */
+const ProtectedRoute = ({ children, allowedRoles = [] }) => {
+  const token = localStorage.getItem('accessToken');
 
-  return isAuthenticated ? children : <Navigate to={ROUTES.CONNEXION} replace />;
+  if (!token) {
+    return <Navigate to={ROUTES.CONNEXION} replace />;
+  }
+
+  try {
+    const decoded = jwtDecode(token);
+    const userRole = decoded.role;
+
+    if (allowedRoles.length === 0 || allowedRoles.includes(userRole)) {
+      return children;
+    } else {
+      return <Navigate to={ROUTES.NOT_FOUND} replace />;
+    }
+  } catch (err) {
+    console.error("Erreur de décodage du token :", err);
+    return <Navigate to={ROUTES.CONNEXION} replace />;
+  }
 };
 
 export default ProtectedRoute;
--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -5,6 +5,18 @@ const ROUTES = {
   INSCRIPTION: '/inscription',
   INSCRIPTION_BAILLEUR: '/inscription-bailleur',
   VALIDATION: '/validation',
+  
+  // Dashboards par rôle
+  DASHBOARD_ADMIN: '/admin/dashboard',
+  DASHBOARD_BAILLEUR: '/bailleur/dashboard',
+  DASHBOARD_ETUDIANT: '/etudiant/accueil',
+
+  //endpoints BAILLEUR
+  ANNONCES_BAILLEUR: '/bailleur/annonces',
+  ANNONCES_EN_ATTENTES: '/bailleur/annonces-attentes',
+  TENANTS: '/tenants',
+
+
   NOT_FOUND: '*',
 };
 
