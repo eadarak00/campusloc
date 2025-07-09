79b9a572d8ad54efbe9acb1aa5b9bfad74bc70c0
eadarak00 <eadarak2001@gmail.com>
2025-07-09 22:53:46 +0000

    refactor: suppression des commentaires

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -1,14 +1,3 @@
-// import { Navigate } from 'react-router-dom';
-// import ROUTES from './routes';
-
-// const ProtectedRoute = ({ children }) => {
-//   const isAuthenticated = !!localStorage.getItem('token');
-
-//   return isAuthenticated ? children : <Navigate to={ROUTES.CONNEXION} replace />;
-// };
-
-// export default ProtectedRoute;
-
 import { Navigate } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
 import ROUTES from './routes';
