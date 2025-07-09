bd8fbaea0aa40c14b69442d9ebbf1411d89908e0
eadarak00 <eadarak2001@gmail.com>
2025-07-09 01:13:19 +0000

    feat: appel d'api pu recuperer une annonce

--- frontend/src/api/annonceAPI.jsx
+++ frontend/src/api/annonceAPI.jsx
@@ -1,27 +1,20 @@
-// src/api/annonce.js
-// import api from "./api";
-
-// const ANNONCE_BASE = "/v1/annonces";
-
-// // Créer une nouvelle annonce
-// export const createAnnonce = (data) => {
-//   return api.post(ANNONCE_BASE, data);
-// };
-
-// export const getAnnoncesEnAttentesParProprietaires = (proprietaireId) => {
-//   return api.get(`${ANNONCE_BASE}/proprietaire/${proprietaireId}/en-attente`);
-// };
-
+// src/api/annonceApi
 import api from "./api";
 
 const ANNONCE_BASE = "/v1/annonces";
 
-// 1️⃣ Créer une annonce
+// Créer une annonce
 export const createAnnonce = (data) => {
   return api.post(ANNONCE_BASE, data);
 };
 
-// 2️⃣ Lister les annonces en attente pour un propriétaire
+// Lister les annonces en attente pour un propriétaire
 export const getAnnoncesEnAttentesParProprietaires = (proprietaireId) => {
   return api.get(`${ANNONCE_BASE}/proprietaire/${proprietaireId}/en-attente`);
 };
+
+// recuperer un annonce de par son ID
+
+export const getAnnonceById = (annonceId) =>{
+  return api.get(`${ANNONCE_BASE}/${annonceId}`);
+}
