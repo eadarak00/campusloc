eb5f0b279c73036cfddeb858363c51db341d946f
eadarak00 <eadarak2001@gmail.com>
2025-07-10 22:41:03 +0000

    feat: ajout de la route

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -1,39 +1,18 @@
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import ROUTES from './routes';
-// import Home from '../pages/Home';
-// import Connexion from '../pages/auth/Connexion';
-// import PublicLayout from '../layouts/PublicLayout';
-// import Inscription from '../pages/auth/Inscription';
-
-// const AppRouter = () => (
-//   <Router>
-//     <Routes>
-//       <Route path="/" element={<PublicLayout />}>
-//         <Route index element={<Home />} />
-//         <Route path={ROUTES.CONNEXION} element={<Connexion />} />
-//         <Route path={ROUTES.INSCRIPTION} element={<Inscription />} />
-//       </Route>
-//     </Routes>
-//   </Router>
-// );
-
-// export default AppRouter;
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ROUTES from './routes';
-import Home from '../pages/Home';
-import Connexion from '../pages/auth/Connexion';
-import PublicLayout from '../layouts/PublicLayout';
-import Inscription from '../pages/auth/Inscription';
+import ROUTES from "./routes";
+import Home from "../pages/Home";
+import Connexion from "../pages/auth/Connexion";
+import PublicLayout from "../layouts/PublicLayout";
+import Inscription from "../pages/auth/Inscription";
 import DashboardBailleur from "../pages/bailleur/DashboardBailleur";
 import BailleurLayout from "../layouts/BailleurLayout";
-import ProtectedRoute from './ProtectedRoute';
+import ProtectedRoute from "./ProtectedRoute";
 import AnnoncesBailleur from "../pages/bailleur/Annonces";
 import CreationAnnonce from "../pages/bailleur/CreationAnnonce";
 import AnnonceEnAttentes from "../pages/bailleur/AnnoncesEnAttente";
 import DetailAnnonce from "../pages/bailleur/AnnonceDetails";
 import ModifierAnnonce from "../pages/bailleur/ModifierAnnonce";
-
+import AnnonceActive from "../pages/bailleur/AnnoncesActives";
 
 const AppRouter = () => (
   <Router>
@@ -54,13 +33,28 @@ const AppRouter = () => (
           </ProtectedRoute>
         }
       >
-        <Route path={ROUTES.DASHBOARD_BAILLEUR} element={<DashboardBailleur />} />
+        <Route
+          path={ROUTES.DASHBOARD_BAILLEUR}
+          element={<DashboardBailleur />}
+        />
         <Route path={ROUTES.ANNONCES_BAILLEUR} element={<AnnoncesBailleur />} />
         <Route path={ROUTES.CREATION_ANNONCE} element={<CreationAnnonce />} />
-        <Route path={ROUTES.ANNONCES_EN_ATTENTES} element={<AnnonceEnAttentes />} />
-        <Route path={ROUTES.DETAIL_ANNONCE_BAILLEUR} element={<DetailAnnonce />} />
-        <Route path={ROUTES.MODIFIER_ANNONCE_BAILLEUR} element={<ModifierAnnonce />} />
-
+        <Route
+          path={ROUTES.ANNONCES_ACTIFS_BAILLEUR}
+          element={<AnnonceActive />}
+        />
+        <Route
+          path={ROUTES.ANNONCES_EN_ATTENTES}
+          element={<AnnonceEnAttentes />}
+        />
+        <Route
+          path={ROUTES.DETAIL_ANNONCE_BAILLEUR}
+          element={<DetailAnnonce />}
+        />
+        <Route
+          path={ROUTES.MODIFIER_ANNONCE_BAILLEUR}
+          element={<ModifierAnnonce />}
+        />
       </Route>
     </Routes>
   </Router>
