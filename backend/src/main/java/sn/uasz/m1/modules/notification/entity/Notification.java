1d8bd0412e3162ebdd7f6c7b619f8e04ca3564da
eadarak00 <eadarak2001@gmail.com>
2025-07-15 23:50:42 +0000

    feat: initialisation automatique des champs lue, dateEnvoie et creerA avant la persistance d'une Notification

--- backend/src/main/java/sn/uasz/m1/modules/notification/entity/Notification.java
+++ backend/src/main/java/sn/uasz/m1/modules/notification/entity/Notification.java
@@ -6,24 +6,34 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import sn.uasz.m1.core.base.BaseEntity;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-public class Notification {
+public class Notification extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String titre;
     private String message;
-    private boolean lue = false;
+    private boolean lue;
+    private LocalDateTime dateEnvoie;
 
-    private LocalDateTime dateCreation = LocalDateTime.now();
+        @PrePersist
+        public void onCreate(){
+            this.lue = false;
+            this.dateEnvoie = LocalDateTime.now();
+            this.setCreerA(LocalDateTime.now());
+        }
+
+
+
 }
