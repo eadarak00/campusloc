df3724ac36b9a6cc2245b2ba46ace24d73c4e904
eadarak00 <eadarak2001@gmail.com>
2025-07-15 23:45:10 +0000

    feat(Notification): création de l'entité Notification avec gestion automatique de l'ID, du titre, du message, du statut de lecture et de la date de création

---
+++ backend/src/main/java/sn/uasz/m1/modules/notification/entity/Notification.java
@@ -0,0 +1,29 @@
+package sn.uasz.m1.modules.notification.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titre;
+    private String message;
+    private boolean lue = false;
+
+    private LocalDateTime dateCreation = LocalDateTime.now();
+}
