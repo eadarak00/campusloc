9fc6eea533c102fa906858cf584865304883b49a
eadarak00 <eadarak2001@gmail.com>
2025-07-18 02:05:51 +0000

    feat: ajout des composants et stylisation

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,14 +2,18 @@
 import React from "react";
 import EnConstruction from "../components/EnConstruction";
 import ParallaxCampusHousing from "../components/Parallax";
-import "../styles/home.css"
 import ProcessStepsComponent from "../components/ProcessComponent";
+import AnnoncesPopulaires from "../components/AnnoncePopulaire";
+
+import "../styles/home.css"
+
 
 const Home = () => {
   return (
     <main className="home__container">
       <ParallaxCampusHousing />
       <ProcessStepsComponent />
+      <AnnoncesPopulaires />
       {/* <EnConstruction nomPage="Accueil" /> */}
     </main>
   );
