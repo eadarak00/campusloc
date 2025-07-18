a8b2d912679ca565a07f137b8d98603c8449efbc
eadarak00 <eadarak2001@gmail.com>
2025-07-18 01:11:24 +0000

    feat: ajout du parallax et styliser le home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 // src/pages/Home.jsx
 import React from "react";
 import EnConstruction from "../components/EnConstruction";
-import Connexion from "./auth/Connexion";
+import ParallaxCampusHousing from "../components/Parallax";
+import "../styles/home.css"
 
 const Home = () => {
   return (
-    <main>
+    <main className="home__container">
+      <ParallaxCampusHousing />
       <EnConstruction nomPage="Accueil" />
     </main>
   );
