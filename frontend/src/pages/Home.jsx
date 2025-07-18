e14dc20b1ec135e12914b17993fec82a1996db39
eadarak00 <eadarak2001@gmail.com>
2025-07-18 01:38:46 +0000

    refactor: changement minimal

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import EnConstruction from "../components/EnConstruction";
 import ParallaxCampusHousing from "../components/Parallax";
 import "../styles/home.css"
+import ProcessStepsComponent from "../components/ProcessComponent";
 
 const Home = () => {
   return (
     <main className="home__container">
       <ParallaxCampusHousing />
-      <EnConstruction nomPage="Accueil" />
+      <ProcessStepsComponent />
+      {/* <EnConstruction nomPage="Accueil" /> */}
     </main>
   );
 };
