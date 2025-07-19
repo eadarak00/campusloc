7f4edb235b7699d28a3fcc6a0521113c33bae96b
eadarak00 <eadarak2001@gmail.com>
2025-07-19 22:14:57 +0000

    feat: recuperation du prospect

--- frontend/src/layouts/ProspectLayout.jsx
+++ frontend/src/layouts/ProspectLayout.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import NavbarProspect from "../components/NavbarProspect";
+import { getProspectFromStorage } from "../utils/authUtils";
 
 const ProspectLayout= () => {
+    const user  = getProspectFromStorage();
   return (
     <div className="d-flex flex-column min-vh-100 publicLayout-main" style={{ backgroundColor: '#FAF9F6', color: '#374151' }}>
-      <NavbarProspect />
+      <NavbarProspect user={user} />
       
       {/* Contenu central */}
       <main className="flex-grow-1">
