ef05eaa35865c8084cd9eeffb100e67f3b68f5e0
eadarak00 <eadarak2001@gmail.com>
2025-07-20 18:02:59 +0000

    feat(dto): ajout de ContactResponseDTO pour exposer les informations de contact

---
+++ backend/src/main/java/sn/uasz/m1/modules/annonce/dto/CoordonneesDTO.java
@@ -0,0 +1,15 @@
+package sn.uasz.m1.modules.annonce.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class CoordonneesDTO {
+    private String nom;
+    private String prenom;
+    private String email;
+    private String telephone;
+}
