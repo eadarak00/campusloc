108b187c730b11220c2f0acf115084491713c392
eadarak00 <eadarak2001@gmail.com>
2025-07-22 22:03:20 +0000

    feat: persistence des donnees de paiement

---
+++ backend/src/main/java/sn/uasz/m1/modules/paiement/repositories/PaiementRepository.java
@@ -0,0 +1,13 @@
+package sn.uasz.m1.modules.paiement.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import sn.uasz.m1.modules.paiement.entities.Paiement;
+
+@Repository
+public interface PaiementRepository extends JpaRepository<Paiement, Long> {
+    Optional<Paiement> findByReference(String reference);
+}
